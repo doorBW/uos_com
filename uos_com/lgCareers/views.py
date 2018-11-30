05eb4ba6ebce599d8faa529fb29685a9903796ae
doorBW <doorbw@outlook.com>
2018-11-30 12:25:14 +0900

    lgcareers for web input func

--- uos_com/lgCareers/views.py
+++ uos_com/lgCareers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from ../NLU/views import MA
+from NLU.views import MA
 # Create your views here.
 def index(request):
 	return 0
