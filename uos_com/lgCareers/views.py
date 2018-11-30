ba3f37e43225381c8083e409a6114f573ecc99cb
doorBW <doorbw@outlook.com>
2018-11-30 11:07:08 +0900

    lgcareers for web input

--- uos_com/lgCareers/views.py
+++ uos_com/lgCareers/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return 0
-	
+
 def getInput(request,input):
-	return '서버 호출 정상'
+	return HttpResponse(input+' 값 제대로 들어옴')
