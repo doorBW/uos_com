0542903bb883224a275a15cb2258f074ce757110
doorBW <doorbw@outlook.com>
2018-11-30 12:35:02 +0900

    lgcareers for web nouns

--- uos_com/NLU/views.py
+++ uos_com/NLU/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from konlpy.tag import Komoran
 # Create your views here.
 def getInput(input):
 	return 0
 
 def MA(input):
+	komoran = Komoran()
 	answer = 'MA함수 호출'
-	
+	nouns = komoran.nouns(input)
+	for i in nouns:
+		answer += i
 	return answer
