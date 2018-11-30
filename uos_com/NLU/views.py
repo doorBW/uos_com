c4fedbee2a1fa98fb60642558a324d4c2f348804
doorBW <doorbw@outlook.com>
2018-11-30 12:42:29 +0900

    lgcareers for web nouns

--- uos_com/NLU/views.py
+++ uos_com/NLU/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from konlpy.tag import Komoran
+import sys
 # Create your views here.
 def getInput(input):
 	return 0
 
 def MA(input):
+	reload(sys)
+    sys.setdefaultencoding('utf-8')
+    input = input.decode('utf-8')
 	komoran = Komoran()
 	answer = 'MA함수 호출'
 	nouns = komoran.nouns(input)
