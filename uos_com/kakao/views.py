ab67498bb9b9cac523f773fd7a0dcf08db6e7106
doorBW <doorbw@outlook.com>
2018-12-03 13:17:25 +0900

    lgcareers for kakao NLU

--- uos_com/kakao/views.py
+++ uos_com/kakao/views.py
@@ -31,7 +31,7 @@ def getMessage(request):
 		content = jsonMessage['content']
 		answer_list = [None, None, None, None, None]
 		if content == (chr(0x2753)+' 어떤 기능이 있나요?'):
-			answer = '현재 다음과 같은 기능을 제공하고 있어요 :)\\n1. 채용공고 안내\\n예시) LG CNS 신입 채용공고 있어?'
+			answer = '현재 다음과 같은 기능을 제공하고 있어요 :)<br>1. 채용공고 안내 \n 예시) LG CNS 신입 채용공고 있어?'
 		elif content == (chr(0x2753)+' 대화 시작할래요!'):
 			answer = '네. 원하시는 기능을 말씀해주세요.'
 		elif conent == (chr(0x2753)+' 문의할게 있어요.'):
