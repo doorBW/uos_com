bfd02bf77a188cccf2b9befdc04ba9ad18092339
BeomwooMoon <beomwoo.moon@linecorp.com>
2021-05-19 18:33:05 +0900

    add csrf exempt

--- uos_com/line/views.py
+++ uos_com/line/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt    #포스트 에러 방지
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
@@ -16,18 +17,23 @@ load_dotenv(dotenv_path)
 channel_access_token = os.getenv('channel_access_token')
 line_bot_api = LineBotApi(channel_access_token)
 
-
+@csrf_exempt
 def webhook(request):
     print(request)
+    return HttpResponse('webhook OK')
 
+@csrf_exempt
 def broadcast(request):
     try:
         line_bot_api.broadcast(TextSendMessage(text='Hello World!'))
     except LineBotApiError as e:
         print(e)
+    return HttpResponse('broadcast OK')
 
+@csrf_exempt
 def broadcast_sise(request):
     try:
         line_bot_api.broadcast(TextSendMessage(text='Hello World!(TODO SISE)'))
     except LineBotApiError as e:
         print(e)
+    return HttpResponse('broadcast_sise OK')
